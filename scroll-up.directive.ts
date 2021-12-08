22e3fc59de09a2936dae12c1193f481bf01b155f
Gevorg Melkonyan <gevor12@gmail.com>
2021-12-08 18:31:22 +0400

    scroll-up update css, html

--- scroll-up.directive.ts
+++ scroll-up.directive.ts
@@ -10,10 +10,12 @@ export class ScrollUpDirective {
 
     constructor(private elementRef: ElementRef, private renderer: Renderer2, private window: Window) { }
 
+    // scrolling the top of the body
     @HostListener('click', ['$event']) onClick(event: any) {
         this.window.scrollTo({ top: 0, behavior: 'smooth' })
     }
 
+    // showing and closing the button of scrolling up, when the scroll height from the top more than 300px
     @HostListener('window:scroll', ['$event']) onScroll(event: any) {
         if (event.target.documentElement.scrollTop > 300) {
             this.renderer.addClass(this.elementRef.nativeElement, 'show')
